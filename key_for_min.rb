957574f855fdfad381d1a205a6f60e45fdcf397c
Mike McNeeley <mmcneeley@gmail.com>
2018-03-03 02:06:43 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,12 +5,12 @@ veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
 
 def key_for_min_value(name_hash)
   nil if name_hash.length == 0
-  #low = nil.to_f
-  #name_hash.each do |key, value|
-  #  if value > low
-  #    low = key
-  #  end
-  #end
+  low = nil.to_f
+  name_hash.each do |key, value|
+    if value < low
+      low = key
+    end
+  end
 end
 
 puts key_for_min_value(veggies)
