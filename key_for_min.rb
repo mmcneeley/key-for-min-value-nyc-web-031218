63f17fcb56e0da0eb76e4311922d8355dda0abda
Mike McNeeley <mmcneeley@gmail.com>
2018-03-03 02:12:46 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-require 'pry'
+#require 'pry'
 
 veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
 
@@ -10,7 +10,7 @@ def key_for_min_value(name_hash)
   low = nil.to_f
   name_hash.each do |key, value|
     if value.to_f < low.to_f
-      binding.pry
+      #binding.pry
       puts "#{low} at #{key} is lower than #{value}"
       low = key
       puts low
