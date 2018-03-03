6a0f94d90694e4ffbf64eff89fd51b2fe7a89de5
Mike McNeeley <mmcneeley@gmail.com>
2018-03-03 01:59:12 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,16 +1,16 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
+
 def key_for_min_value(name_hash)
-  low_value = nil
-  name_hash.each do |key, value|
-    if low_value != nil && value < low_value
-      low_value = key
-    elsif low_value == nil
-      low_value = key
-    else
-      low_value
-    end
-  end
-  low_value
+  "Mike" if name_hash.length == 0
+  #low = nil.to_f
+  #name_hash.each do |key, value|
+  #  if value > low
+  #    low = key
+  #  end
+  #end
 end
+
+puts key_for_min_value(veggies)
